1a4fd7fde7e8e52b6a8f01e2cc02a61cc78e134d
unknown <jepperm@gmail.com>
2013-07-25 19:40:05 +0200

    Opera legacy extension now has cross-origin xhr.

---
+++ src/.opera/scripts/background.js
@@ -0,0 +1,76 @@
+function xhr(details) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function(){
+    var responseState = {
+      responseXML:(xmlhttp.readyState == 4 ? xmlhttp.responseXML : ''),
+      responseText:(xmlhttp.readyState == 4 ? xmlhttp.responseText : ''),
+      readyState:xmlhttp.readyState,
+      responseHeaders:(xmlhttp.readyState == 4 ? xmlhttp.getAllResponseHeaders() : ''),
+      status:(xmlhttp.readyState == 4 ? xmlhttp.status : 0),
+      statusText:(xmlhttp.readyState == 4 ? xmlhttp.statusText : '')
+    };
+    if (details["onreadystatechange"]) {
+      details["onreadystatechange"](responseState);
+    }
+    if (xmlhttp.readyState == 4) {
+      if (details["onload"] && xmlhttp.status >= 200 && xmlhttp.status < 300) {
+        details["onload"](responseState);
+      }
+      if (details["onerror"] && (xmlhttp.status < 200 || xmlhttp.status >= 300)) {
+        details["onerror"](responseState);
+      }
+    }
+  };
+  try {
+    xmlhttp.open(details.method, details.url);
+  } catch(e) {
+    if(details["onerror"]) {
+      details["onerror"]({responseXML:'',responseText:'',readyState:4,responseHeaders:'',status:403,statusText:'Forbidden'});
+    }
+    return false;
+  }
+  if (details.headers) {
+    for (var prop in details.headers) {
+      xmlhttp.setRequestHeader(prop, details.headers[prop]);
+    }
+  }
+  xmlhttp.send((typeof(details.data) != 'undefined') ? details.data : null);
+  return true
+}
+
+opera.extension.onmessage = function(e) {
+  if (e.data.action === "xhr") {
+    var id = e.data.id,
+        details = e.data.details;
+    if (details.onreadystatechange) {
+      details.onreadystatechange = function(response){
+        e.source.postMessage({
+          action: 'xhr onreadystatechange',
+          id: id,
+          response: response
+        });
+      };
+    }
+    if (details.onload) {
+      details.onload = function(response){
+        e.source.postMessage({
+          action: 'xhr onload',
+          id: id,
+          response: response
+        });
+      };
+    }
+    if (details.onerror) {
+      details.onerror = function(response){
+        e.source.postMessage({
+          action: 'xhr onerror',
+          id: id,
+          response: response
+        });
+      };
+    }
+    xhr(details);
+  } else {
+    console.error("[Opera background.js] Unknown action (" + e.data.action + ")");
+  }
+};
