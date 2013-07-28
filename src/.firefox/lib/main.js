a29de9dc20fb5fb959dbe35e7785c9de5b40011f
unknown <jepperm@gmail.com>
2013-07-28 20:43:38 +0200

    Added indents to Video Thumbnail options. Fixed code typo (Firefox caught it).

--- src/.firefox/lib/main.js
+++ src/.firefox/lib/main.js
@@ -46,27 +46,5 @@ page = PageMod({
       }
       xhr(data.details);
     });
-    /*
-    worker.port.on("xhr", function(details){
-      try {
-        if (details.onload) {
-          details.onComplete = function(response){
-            worker.port.emit("xhr_onload", JSON.stringify({id: details.__id, text: response.text}));
-          };
-        }
-        if (details.onerror) {
-          details.onerror = function(response){
-            worker.port.emit("xhr_onerror", JSON.stringify({id: details.__id, text: response.text}));
-          };
-        }
-        if (details.onreadystatechange) {
-          details.onreadystatechange = function(response){
-            worker.port.emit("xhr_onreadystatechange", JSON.stringify({id: details.__id, text: response.text}));
-          };
-        }
-        xhr(details);
-      }
-      console.log(details);
-    });*/
   }
 });
