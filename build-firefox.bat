831b3e59cd6392453d8fd6208fbae00a7b92793c
unknown <jepperm@gmail.com>
2013-07-29 18:00:38 +0200

    Build everything.

--- build-firefox.bat
+++ build-firefox.bat
@@ -1,7 +1,7 @@
 CALL SDK\firefox-addon-sdk-1.14\bin\activate.bat
 
 cd build\YouTubeCenter
-CALL cfx xpi --force-mobile
+CALL cfx xpi
 
 move youtube-center.xpi ..\..\dist\YouTubeCenter.xpi
 cd ..\..\
