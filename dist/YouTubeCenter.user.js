6db625555cdcd652411a16ee3fefd693755b9591
unknown <jepperm@gmail.com>
2013-12-03 18:06:20 +0100

    Disabled comment filtering. Added catching of errors in the embedded videos part of YouTube Center to insure that YouTube Center always will write the player.

--- dist/YouTubeCenter.user.js
+++ dist/YouTubeCenter.user.js
@@ -2473,96 +2473,117 @@
       ytcenter.embed._writeEmbed = uw.writeEmbed;
       ytcenter.embed.isYouTubeReady = false;
       ytcenter.embed.isYouTubeCenterReady = false;
+      ytcenter.embed.failsafe = false;
       defineLockedProperty(uw, "writeEmbed", function(func){
         con.log("[Embed] writeEmbed has been leaked to YouTube Center.");
         ytcenter.embed._writeEmbed = func;
       }, function(){
         return function(){
           con.log("[Embed] YouTube has called writeEmbed.");
-          ytcenter.embed.isYouTubeReady = true;
-          if (ytcenter.embed.writePlayer)
-            ytcenter.embed.writePlayer();
+          if (ytcenter.embed.failsafe) {
+            ytcenter.embed._writeEmbed();
+          } else {
+            ytcenter.embed.isYouTubeReady = true;
+            if (ytcenter.embed.writePlayer)
+              ytcenter.embed.writePlayer();
+          }
         };
       });
       
       ytcenter.embed.writePlayer = function(){
-        if (!ytcenter.embed._writeEmbed) {
-          con.log("[Embed] writeEmbed is not yet ready!");
-          return;
+        try {
+          if (!ytcenter.embed._writeEmbed) {
+            con.log("[Embed] writeEmbed is not yet ready!");
+            return;
+          }
+          con.log("[Embed] Checking if YouTube and YouTube Center are ready...");
+          if (!ytcenter.embed.isYouTubeReady || !ytcenter.embed.isYouTubeCenterReady) {
+            con.log("[Embed] They're both not ready yet!");
+            return;
+          }
+          /* Settings the player config according to YouTube Center */
+          var cfg = ytcenter.player.getConfig();
+          if (cfg) uw.yt.config_.PLAYER_CONFIG = cfg;
+          
+          /* Writing the embedded player */
+          con.log("[Embed] Writing the embedded player.");
+          ytcenter.embed._writeEmbed();
+        } catch (e) {
+          con.error(e);
+          ytcenter.embed.failsafe = true;
+          /* Trying to write the player when an error was thrown */
+          con.log("[Embed] Writing the embedded player.");
+          ytcenter.embed._writeEmbed();
         }
-        con.log("[Embed] Checking if YouTube and YouTube Center are ready...");
-        if (!ytcenter.embed.isYouTubeReady || !ytcenter.embed.isYouTubeCenterReady) {
-          con.log("[Embed] They're both not ready yet!");
-          return;
-        }
-        /* Settings the player config according to YouTube Center */
-        var cfg = ytcenter.player.getConfig();
-        if (cfg) uw.yt.config_.PLAYER_CONFIG = cfg;
-        
-        /* Writing the embedded player */
-        con.log("[Embed] Writing the embedded player.");
-        ytcenter.embed._writeEmbed();
       };
       
       ytcenter.embed.load = function(){
-        con.log("[Embed] Loading video data...");
-        var url = ytcenter.player.getVideoDataRequest();
-        
-        con.log("[Embed] Downloading data from " + url);
-        ytcenter.utils.xhr({
-          method: "GET",
-          url: url,
-          headers: {
-            "Content-Type": "text/plain"
-          },
-          onload: function(response){
-            try {
-              if (response.responseText) {
-                con.log("[Embed] Download complete.");
-                var object = {}, tokens = response.responseText.split("&");
-                for (var i = 0; i < tokens.length; i++) {
-                  var ss = tokens[i].split("=");
-                  object[ss[0]] = decodeURIComponent(ss[1]);
-                }
-                if (object.errorcode) {
-                  con.error("[Embed] Error: " + object.errorcode + ": " + object.reason);
-                } else {
-                  if (object.dash) ytcenter.player.config.args.dash = object.dash;
-                  if (object.dashmpd) ytcenter.player.config.args.dashmpd = object.dashmpd;
-                  if (object.adaptive_fmts) ytcenter.player.config.args.adaptive_fmts = object.adaptive_fmts;
-                  if (object.fmt_list) ytcenter.player.config.args.fmt_list = object.fmt_list;
-                  if (object.url_encoded_fmt_stream_map) ytcenter.player.config.args.url_encoded_fmt_stream_map = object.url_encoded_fmt_stream_map;
-                  if (object.url_encoded_fmt_stream_map || object.adaptive_fmts) {
-                    ytcenter.video.streams = ytcenter.parseStreams(object);
+        try {
+          con.log("[Embed] Loading video data...");
+          var url = ytcenter.player.getVideoDataRequest();
+          
+          con.log("[Embed] Downloading data from " + url);
+          ytcenter.utils.xhr({
+            method: "GET",
+            url: url,
+            headers: {
+              "Content-Type": "text/plain"
+            },
+            onload: function(response){
+              try {
+                if (response.responseText) {
+                  con.log("[Embed] Download complete.");
+                  var object = {}, tokens = response.responseText.split("&");
+                  for (var i = 0; i < tokens.length; i++) {
+                    var ss = tokens[i].split("=");
+                    object[ss[0]] = decodeURIComponent(ss[1]);
                   }
-                  ytcenter.player.setConfig(ytcenter.player.modifyConfig("embed", ytcenter.player.config));
+                  if (object.errorcode) {
+                    con.error("[Embed] Error: " + object.errorcode + ": " + object.reason);
+                  } else {
+                    if (object.dash) ytcenter.player.config.args.dash = object.dash;
+                    if (object.dashmpd) ytcenter.player.config.args.dashmpd = object.dashmpd;
+                    if (object.adaptive_fmts) ytcenter.player.config.args.adaptive_fmts = object.adaptive_fmts;
+                    if (object.fmt_list) ytcenter.player.config.args.fmt_list = object.fmt_list;
+                    if (object.url_encoded_fmt_stream_map) ytcenter.player.config.args.url_encoded_fmt_stream_map = object.url_encoded_fmt_stream_map;
+                    if (object.url_encoded_fmt_stream_map || object.adaptive_fmts) {
+                      ytcenter.video.streams = ytcenter.parseStreams(object);
+                    }
+                    ytcenter.player.setConfig(ytcenter.player.modifyConfig("embed", ytcenter.player.config));
+                  }
+                  
+                  ytcenter.embed.isYouTubeCenterReady = true;
+                  ytcenter.embed.writePlayer();
+                } else {
+                  con.error("[Embed] Didn't receive any data!");
+                  ytcenter.embed.failsafe = true;
+                  /* Going to set YouTube Center as ready to make it possible for the user to watch the embedded video if possible. */
+                  ytcenter.embed.isYouTubeCenterReady = true;
+                  ytcenter.embed.writePlayer();
                 }
-                
-                ytcenter.embed.isYouTubeCenterReady = true;
-                ytcenter.embed.writePlayer();
-              } else {
-                con.error("[Embed] Didn't receive any data!");
-                /* Going to set YouTube Center as ready to make it possible for the user to watch the embedded video if possible. */
+              } catch (e) {
+                con.error(e);
+                ytcenter.embed.failsafe = true;
+                /* Just to make people happy. */
                 ytcenter.embed.isYouTubeCenterReady = true;
                 ytcenter.embed.writePlayer();
               }
-            } catch (e) {
-              con.error(e);
-              
-              /* Just to make people happy. */
+            },
+            onerror: function(){
+              con.error("[Embed] Connection failed!");
+              ytcenter.embed.failsafe = true;
+              /* Going to set YouTube Center as ready to make it possible for the user to watch the embedded video if possible. */
               ytcenter.embed.isYouTubeCenterReady = true;
               ytcenter.embed.writePlayer();
             }
-          },
-          onerror: function(){
-            con.error("[Embed] Connection failed!");
-            /* Going to set YouTube Center as ready to make it possible for the user to watch the embedded video if possible. */
-            ytcenter.embed.isYouTubeCenterReady = true;
-            ytcenter.embed.writePlayer();
-          }
-        });
+          });
+        } catch (e) {
+          con.error(e);
+          ytcenter.embed.failsafe = true;
+          ytcenter.embed.isYouTubeCenterReady = true;
+          ytcenter.embed.writePlayer();
+        }
       };
-      
     } catch (e) {
       con.error(e);
     }
@@ -3698,14 +3719,14 @@
       
       __r.update = function(){
         __r.loadComments();
-        __r.filter();
-        __r.commentDuplicates();
+        /*__r.filter();
+        __r.commentDuplicates();*/
         __r.addFlags();
       };
       __r.setupObserver = function(){
         try {
           observer = ytcenter.mutation.observe(document.body, { childList: true, subtree: true }, function(){
-            con.log("[Comment Observer] In action!");
+            con.log("[Comment Observer] Mutation Observed!");
             __r.update();
           });
         } catch (e) {
@@ -20265,7 +20286,7 @@
         inject(main_function);
       } else {
         //try {
-          main_function(false, 0, true, 116);
+          main_function(false, 0, true, 117);
         /*} catch (e) {
         }*/
       }
@@ -20285,7 +20306,7 @@
     }
   } else {
     //try {
-      main_function(false, 0, true, 116);
+      main_function(false, 0, true, 117);
     //} catch (e) {
       //console.error(e);
     //}
