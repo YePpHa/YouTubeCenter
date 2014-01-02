ddf5d1c540bea6265c949dd3edab1aab4f3dc2cc
unknown <jepperm@gmail.com>
2014-01-02 02:25:39 +0100

    chrome-update.xml was pointing at itself. Now it will point to the crx file.

--- dist/YouTubeCenter.user.js
+++ dist/YouTubeCenter.user.js
@@ -23,7 +23,7 @@
 // ==UserScript==
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         184
+// @version         185
 // @author          Jeppe Rune Mortensen (YePpHa)
 // @description     YouTube Center contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/logo-48x48.png
@@ -77,7 +77,7 @@
       if (typeof func === "string") {
         func = "function(){" + func + "}";
       }
-      script.appendChild(document.createTextNode("(" + func + ")(true, 0, true, 184);\n//# sourceURL=YouTubeCenter.js"));
+      script.appendChild(document.createTextNode("(" + func + ")(true, 0, true, 185);\n//# sourceURL=YouTubeCenter.js"));
       p.appendChild(script);
       p.removeChild(script);
     } catch (e) {}
@@ -21890,7 +21890,7 @@
         inject(main_function);
       } else {
         //try {
-          main_function(false, 0, true, 184);
+          main_function(false, 0, true, 185);
         /*} catch (e) {
         }*/
       }
@@ -21910,7 +21910,7 @@
     }
   } else {
     //try {
-      main_function(false, 0, true, 184);
+      main_function(false, 0, true, 185);
     //} catch (e) {
       //console.error(e);
     //}
