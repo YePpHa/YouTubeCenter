acac2b392474a3dd6c0cafe66ff9913d2cc20f10
unknown <jepperm@gmail.com>
2014-01-04 21:24:22 +0100

    ...

--- dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
+++ dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
@@ -23,7 +23,7 @@
 // ==UserScript==
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         201
+// @version         202
 // @author          Jeppe Rune Mortensen (YePpHa)
 // @description     YouTube Center contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/logo-48x48.png
@@ -77,7 +77,7 @@
       if (typeof func === "string") {
         func = "function(){" + func + "}";
       }
-      script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 201);\n//# sourceURL=YouTubeCenter.js"));
+      script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 202);\n//# sourceURL=YouTubeCenter.js"));
       p.appendChild(script);
       p.removeChild(script);
     } catch (e) {}
@@ -4865,12 +4865,13 @@
           try {
             data = JSON.parse(r.responseText);
           } catch (e) {
+            con.error("[Comments getUserData] Couldn't parse data from https://gdata.youtube.com/feeds/api/users/" + userId + "?alt=json");
             con.error(e);
           }
           callback(data);
         },
         onerror: function(){
-          con.error("[Comments getGooglePlusUserData] Couldn't fetch data from https://gdata.youtube.com/feeds/api/users/" + userId + "?alt=json");
+          con.error("[Comments getUserData] Couldn't fetch data from https://gdata.youtube.com/feeds/api/users/" + userId + "?alt=json");
           callback(null);
         }
       });
@@ -4895,6 +4896,8 @@
               callback(null);
             }
           } catch (e) {
+            con.error("[Comments getGooglePlusUserData] Couldn't parse data from http://www.youtube.com/profile_redirector/" + oId);
+            con.error(r);
             con.error(e);
             callback(null);
           }
@@ -22011,7 +22014,7 @@
         inject(main_function);
       } else {
         //try {
-          main_function(false, 4, true, 201);
+          main_function(false, 4, true, 202);
         /*} catch (e) {
         }*/
       }
@@ -22031,7 +22034,7 @@
     }
   } else {
     //try {
-      main_function(false, 4, true, 201);
+      main_function(false, 4, true, 202);
     //} catch (e) {
       //console.error(e);
     //}
