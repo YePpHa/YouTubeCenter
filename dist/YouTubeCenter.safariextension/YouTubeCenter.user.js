5ec220363c6d3f9b614b467abf49e92793fc2200
unknown <jepperm@gmail.com>
2014-01-15 20:53:25 +0100

    Increased the build number due to forgetting to do it for the latest commit.

--- dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
+++ dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
@@ -24,7 +24,7 @@
 // @id              YouTubeCenter
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         235
+// @version         236
 // @author          Jeppe Rune Mortensen <jepperm@gmail.com>
 // @description     YouTube Center contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/logo-48x48.png
@@ -84,7 +84,7 @@
       if (typeof func === "string") {
         func = "function(){" + func + "}";
       }
-      script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 235);\n//# sourceURL=YouTubeCenter.js"));
+      script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 236);\n//# sourceURL=YouTubeCenter.js"));
       p.appendChild(script);
       p.removeChild(script);
     } catch (e) {}
@@ -22893,7 +22893,7 @@
         inject(main_function);
       } else {
         //try {
-          main_function(false, 4, true, 235, crossUnsafeWindow);
+          main_function(false, 4, true, 236, crossUnsafeWindow);
         /*} catch (e) {
         }*/
       }
@@ -22912,6 +22912,6 @@
       inject(main_function);
     }
   } else {
-    main_function(false, 4, true, 235, crossUnsafeWindow);
+    main_function(false, 4, true, 236, crossUnsafeWindow);
   }
 })();
