f42fe5645fef7d3139e06f9cb911ae078f926607
unknown <jepperm@gmail.com>
2014-02-20 04:39:33 +0100

    This seems to have fixed the memory leak in the Firefox Extension. This needs some more testing to make sure it's completely fixed.

--- src/.firefox/modules/utils/console.js
+++
@@ -1,7 +0,0 @@
-const EXPORTED_SYMBOLS = ['console'];
-const conService = Components.classes['@mozilla.org/consoleservice;1'].getService(Components.interfaces.nsIConsoleService);
-
-const console = {
-  log: function(a, b, c, d, e){ conService.logStringMessage(a, b, c, d, e); },
-  error: function(a, b, c, d, e){ Components.utils.reportError(a, b, c, d, e); }
-};
