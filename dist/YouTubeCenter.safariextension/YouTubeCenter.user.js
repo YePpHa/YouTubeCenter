34a814f2d897b518f8e7aae79cfc6fc23b8df6bd
YePpHa <jepperm@gmail.com>
2014-07-06 04:37:34 +0200

    Added a suffix to the quality option in the player glow section.

--- dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
+++ dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
@@ -24,7 +24,7 @@
 // @id              YouTubeCenter
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         332
+// @version         333
 // @author          Jeppe Rune Mortensen <jepperm@gmail.com>
 // @description     YouTube Center contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/logo-48x48.png
@@ -86,7 +86,7 @@
       if (typeof func === "string") {
         func = "function(){" + func + "}";
       }
-      script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 332);\n//# sourceURL=YouTubeCenter.js"));
+      script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 333);\n//# sourceURL=YouTubeCenter.js"));
       p.appendChild(script);
       p.removeChild(script);
     } catch (e) {}
@@ -15589,7 +15589,8 @@
             "SETTINGS_PLAYERGLOW_FACTOR", // label
             {
               "min": 1,
-              "max": 100
+              "max": 100,
+              "suffix": "%"
             }
           );
           ytcenter.events.addEvent("settings-update", function(){
@@ -24447,7 +24448,7 @@
         
         inject(main_function);
       } else {
-        main_function(false, 4, true, 332, crossUnsafeWindow);
+        main_function(false, 4, true, 333, crossUnsafeWindow);
       }
     } catch (e) {
       window.addEventListener("message", function(e){
@@ -24510,6 +24511,6 @@
     
     inject(main_function);
   } else {
-    main_function(false, 4, true, 332, crossUnsafeWindow);
+    main_function(false, 4, true, 333, crossUnsafeWindow);
   }
 })();
