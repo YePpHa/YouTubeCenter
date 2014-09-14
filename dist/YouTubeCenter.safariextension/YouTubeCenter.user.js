337195f05a592af173654e513c6b6404da49da32
YePpHa <jepperm@gmail.com>
2014-09-14 16:08:17 +0200

    Fixed #379.

--- dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
+++ dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
@@ -24,7 +24,7 @@
 // @id              YouTubeCenter
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         392
+// @version         393
 // @author          Jeppe Rune Mortensen <jepperm@gmail.com>
 // @description     YouTube Center Developer Build contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/icon48.png
@@ -90,7 +90,7 @@
     if (typeof func === "string") {
       func = "function(){" + func + "}";
     }
-    script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 392);\n//# sourceURL=YouTubeCenter.js"));
+    script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 393);\n//# sourceURL=YouTubeCenter.js"));
     p.appendChild(script);
     p.removeChild(script);
   }
@@ -24518,7 +24518,9 @@
       
     } else {
       console.warn("[Chrome] Chrome extension API is not present!");
-      defaultSave(id, key, data);
+      setTimeout(function(){
+        defaultSave(id, key, data);
+      }, 7);
     }
   }
 
@@ -24547,7 +24549,9 @@
       }
     } else {
       console.warn("[Chrome] Chrome extension API is not present!");
-      defaultLoad(id, key);
+      setTimeout(function(){
+        defaultLoad(id, key);
+      }, 7);
     }
   }
 
@@ -24673,7 +24677,9 @@
         break;
       case "GM_registerMenuCommand":
         if (support.Greasemonkey) {
-          GM_registerMenuCommand(data.arguments[0], bind(null, callUnsafeWindow, data.id));
+          setTimeout(function(){
+            GM_registerMenuCommand(data.arguments[0], bind(null, callUnsafeWindow, data.id));
+          }, 7);
         }
         break;
       default:
@@ -24762,7 +24768,9 @@
     if (4 === 6) { // Firefox
       request(details);
     } else if (support.Greasemonkey) {
-      GM_xmlhttpRequest(details);
+      setTimeout(function(){
+        GM_xmlhttpRequest(details);
+      }, 7);
     } else {
       var xmlhttp;
       if (typeof XMLHttpRequest != "undefined") {
@@ -24827,7 +24835,9 @@
     } else if (4 === 6) {
       callUnsafeWindow(id, storage_setValue(key, data));
     } else {
-      defaultSave(id, key, data);
+      setTimeout(function(){
+        defaultSave(id, key, data);
+      }, 7);
     }
   }
   
@@ -24849,7 +24859,9 @@
     } else if (4 === 6) {
       callUnsafeWindow(id, storage_getValue(key) || "{}");
     } else {
-      defaultLoad(id, key);
+      setTimeout(function() {
+        defaultLoad(id, key);
+      }, 7);
     }
   }
   
