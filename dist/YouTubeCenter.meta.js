00afa635611b814ed5179769d1d0ca9930fec056
YePpHa <jepperm@gmail.com>
2014-10-16 20:47:27 +0200

    Added a conditional check to prevent YouTube Center from not working.

--- dist/YouTubeCenter.meta.js
+++ dist/YouTubeCenter.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         405
+// @version         406
 // @author          Jeppe Rune Mortensen (YePpHa)
 // @description     YouTube Center contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/logo-48x48.png
@@ -39,7 +39,7 @@
 // @grant           GM_registerMenuCommand
 // @updateURL       https://github.com/YePpHa/YouTubeCenter/raw/master/dist/YouTubeCenter.meta.js
 // @downloadURL     https://github.com/YePpHa/YouTubeCenter/raw/master/dist/YouTubeCenter.user.js
-// @updateVersion   153
+// @updateVersion   155
 // @run-at          document-start
 // @priority        9001
 // ==/UserScript==
