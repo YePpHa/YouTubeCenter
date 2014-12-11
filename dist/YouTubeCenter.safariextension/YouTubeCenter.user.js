5e0f7f0e5a97e4c804e8bc3e8458fbe0fa956b50
YePpHa <jepperm@gmail.com>
2014-12-11 17:59:57 +0100

    Build

--- dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
+++ dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
@@ -24,7 +24,7 @@
 // @id              YouTubeCenter
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         450
+// @version         451
 // @author          Jeppe Rune Mortensen <jepperm@gmail.com>
 // @description     YouTube Center Developer Build contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/icon48.png
@@ -98,7 +98,7 @@
     if (typeof func === "string") {
       func = "function(){" + func + "}";
     }
-    script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 450);\n//# sourceURL=YouTubeCenter.js"));
+    script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 451);\n//# sourceURL=YouTubeCenter.js"));
     p.appendChild(script);
     p.removeChild(script);
   }
@@ -4791,7 +4791,7 @@
         ytcenter.page = "channel";
       } else if (document.getElementById("page") && ytcenter.utils.hasClass(document.getElementById("page"), "channel")) {
         ytcenter.page = "channel";
-      } else if (!!url.match(/^http(s)?:\/\/(www\.)?youtube\.com\/(user|channel)\//)) {
+      } else if (!!url.match(/^http(s)?:\/\/(www\.)?youtube\.com\/(user|channel|u|c)\//)) {
         ytcenter.page = "channel";
       } else if (!!url.match(/^http(s)?:\/\/(www\.)?youtube\.com\//)) {
         if (loc.pathname === "/results") {
