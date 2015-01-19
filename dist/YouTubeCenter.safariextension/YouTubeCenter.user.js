94a855105b84ecd05df6fb4a9a177b3ce67c5dd0
YePpHa <jepperm@gmail.com>
2015-01-19 18:04:36 +0100

    Fixed #1371.

--- dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
+++ dist/YouTubeCenter.safariextension/YouTubeCenter.user.js
@@ -24,7 +24,7 @@
 // @id              YouTubeCenter
 // @name            YouTube Center Developer Build
 // @namespace       http://www.facebook.com/YouTubeCenter
-// @version         465
+// @version         466
 // @author          Jeppe Rune Mortensen <jepperm@gmail.com>
 // @description     YouTube Center Developer Build contains all kind of different useful functions which makes your visit on YouTube much more entertaining.
 // @icon            https://raw.github.com/YePpHa/YouTubeCenter/master/assets/icon48.png
@@ -98,7 +98,7 @@
     if (typeof func === "string") {
       func = "function(){" + func + "}";
     }
-    script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 465);\n//# sourceURL=YouTubeCenter.js"));
+    script.appendChild(document.createTextNode("(" + func + ")(true, 4, true, 466);\n//# sourceURL=YouTubeCenter.js"));
     p.appendChild(script);
     p.removeChild(script);
   }
@@ -20188,7 +20188,7 @@
         updateLikedTint();
         updateDislikedTint();
         updateLikedHoverTint();
-        updateDislikeHoverTint();
+        updateDislikedHoverTint();
       }
       
       function updateLikeTint() {
